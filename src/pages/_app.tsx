fa01f550ddcb5cb139837a1fb118a83cc9bc9ac8
Ömer Esmer <oesmer437@gmail.com>
2021-11-24 18:22:50 +0300

    first Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
-import "../styles/globals.css";
+import "@assets/styles/globals.css";
 import "tailwindcss/tailwind.css";
-
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import store from "../store/store";
 import { createWrapper } from "next-redux-wrapper";
+import store from "../store/store";
+// import Layout from "@layout/Layout";
+import "@lib/Icons";
+import "react-toastify/dist/ReactToastify.css";
+
+// import { isLoggedIn } from "store/actions/userActions";
+
+// import ToasterContainer from "@components/ToasterContainer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
